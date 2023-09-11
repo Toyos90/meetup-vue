ab0c17a714a9cc99b10a8b742042352f5e86a9c4
Nafisa <naus7568@gmail.com>
2023-09-11 23:39:37 +0200

    Added footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import NavbarComponent from '../components/NavbarComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
 </script>
 
 <template>
   <main>
     <NavbarComponent />
+    <FooterComponent />
   </main>
 </template>
