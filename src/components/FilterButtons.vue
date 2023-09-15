8d692b40eb9514d40bfcf0b57d4221cd60fc257c
LuLu <lu.luzan@gmail.com>
2023-09-15 19:05:13 +0200

    filter_ok

--- src/components/FilterButtons.vue
+++ src/components/FilterButtons.vue
@@ -11,16 +11,16 @@
 
 <template>
     <div class="btn_container">
-      <v-btn @click="filterData('todos')" :class="{ 'selected': filterCategory === 'todos' }">Todos</v-btn>
-      <v-btn @click="filterData('Perros')" :class="{ 'selected': filterCategory === 'Perros' }">Perros</v-btn>
-      <v-btn @click="filterData('Gatos')" :class="{ 'selected': filterCategory === 'Gatos' }">Gatos</v-btn>
+      <v-btn @click="filterData('todos')">Todos</v-btn>
+      <v-btn @click="filterData('perros')">Perros</v-btn>
+      <v-btn @click="filterData('gatos')">Gatos</v-btn>
     </div>
   </template>
   
- 
-  <style scoped>
+  
 
-  .btn-container {
+  <style scoped>
+   .btn-container {
     display: flex;
     justify-content: center;
     gap: 10px; 
@@ -29,8 +29,6 @@
   .selected {
     background-color: #8B5A2B;
     color: white;
-    border-radius: 10px; }
-  
+    border-radius: 10px; 
+  }
   </style>
-
-  
