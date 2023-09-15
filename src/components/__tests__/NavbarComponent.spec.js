e806b698b9e46722ccaaaa75d359820293df20b4
javier <ertoyos@gmail.com>
2023-09-15 15:34:54 +0200

    add coments

--- src/components/__tests__/NavbarComponent.spec.js
+++ src/components/__tests__/NavbarComponent.spec.js
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import HomeView from '../../views/HomeView.vue'
 import NavbarComponent from '../NavbarComponent.vue'
-
+//test para demostrar que se renderiza la navbar en la HomeView.
 describe('HomeView', () => {
   it('renders NavbarComponent', () => {
     const wrapper = mount(HomeView)
